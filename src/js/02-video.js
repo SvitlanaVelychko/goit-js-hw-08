c50b8b013ceefacdea899ed1b1290868fddf106a
SvitlanaVelychko <svel2608@gmail.com>
2022-06-10 01:22:06 +0300

    Solve tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,9 +7,9 @@ const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 player.on('timeupdate', throttle(onVideoCurrentTime, 1000));
 
+const currentTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+player.setCurrentTime(currentTime);
+
 function onVideoCurrentTime(data) {
     localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
 }
-
-const currentTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-player.setCurrentTime(currentTime);
